f8b0e6008dd07ab566c78d76c53f2395b87f87ec
RensGits <somzoastudios@gmail.com>
2021-03-17 15:24:20 +0100

    Added home page animation, layout detailing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import Home from './Pages/Home';
-import Projects from './Pages/Projects';
+import About from './Pages/About';
 import Contact from './Pages/Contact';
-import Nav from './Pages/Components/Nav';
+import Nav from './Pages/Components/Navigationbar/Nav'
+import './Pages/Components/Navigationbar/Nav.css';
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 
 
 function App() {
+
+  
   return (
-    
+    <AnimatePresence>
     <Router  >
       <div className = 'appGrid' >
         <Nav/>
         <Switch>
           <Route path = "/" exact component = {Home} />
-          <Route path = '/projects' component = {Projects} />
+          <Route path = '/about' component = {About} />
           <Route path = '/contact' component = {Contact} />
         </Switch>
       </div>
     </Router>
-    
+    </AnimatePresence> 
   );
 }
 
